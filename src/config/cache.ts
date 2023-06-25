b4c06f118381edb0f7d201db3aad89b7b3ea86ea
Prosper <anosikeprosper2@gmail.com>
2023-06-25 07:56:37 +0100

    redis cache file

---
+++ src/config/cache.ts
@@ -0,0 +1,25 @@
+import { createClient, RedisClientType } from 'redis';
+import config from './config'
+
+
+function RedisDb () {
+    let client: RedisClientType;
+
+    return async function RedisDb(){
+        console.log(client)
+        if(client) return client; 
+
+        client = createClient({ url: config.REDIS ?? "" });
+
+        await client.connect();
+        client.on('error', (err: any) => console.log('Redis Client Error', err));
+        client.on("connect", () => {
+            console.log('Redis connection successfl')
+        })
+        return client
+    }
+}
+
+export default RedisDb();
+
+
