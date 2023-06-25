35d004cfaed9d3930e6e240db28420be7b0f5f33
Prosper <anosikeprosper2@gmail.com>
2023-06-25 07:58:06 +0100

    mongodb database connection file

---
+++ src/config/db.ts
@@ -0,0 +1,21 @@
+// const mongoose = require("mongoose");
+
+import mongoose from "mongoose";
+import config from "./config";
+
+const MONGO_URL = config.Mongoose_URL;
+console.log('mongoooooooooooo',MONGO_URL)
+export function connectToMongoDB() {
+  mongoose.set("strictQuery", false);
+
+  mongoose
+    .connect(MONGO_URL)
+    .then(() => {
+      console.log("Connection to MongoDB successful");
+    })
+    .catch((err: Error) => {
+      console.log("Connection to MongoDB failed", err);
+    });
+    
+}
+
