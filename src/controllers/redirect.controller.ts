bd6be08ded0680f12c244be7dd507a35f0f1d985
Prosper <anosikeprosper2@gmail.com>
2023-06-26 09:52:49 +0100

    removed console.log

--- src/controllers/redirect.controller.ts
+++ src/controllers/redirect.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { getLongUrl, findUrl } from "../services/url.services";
 import cache from "../config/cache";
+import { AppError } from "../errors/appError";
 
 const redirectToUrl = async (
   req: Request,
@@ -13,23 +14,24 @@ const redirectToUrl = async (
     if (req.params.shortcode) {
       const ip = req.ip;
       const agent = req.headers["user-agent"];
+      
       let url;
-      url = await cacheClient.hGet("", req.params.shortcode);
+      url = await cacheClient.hGet("", req.params.shortcode); // check if shortcode is in the cache
       if (!url) {
         const res = await findUrl(req.params.shortcode);
-        console.log(res)
+      
         url = res?.longurl;
         if(!url)
-          throw new Error("adsafd");
+          throw new AppError('Invalid shortcode', 404);
 
         await cacheClient.hSet("", req.params.shortcode, url ?? "");
       }
       res.redirect(url as string);
       await getLongUrl(req.params.shortcode, ip, agent);
-      console.log('abyss')
+    
     }
   } catch (err) {
-    console.log("errrrorrrrrrrrrr", err);
+    
     next(err);
   }
 };
