0541a33e60903de82f77db4e274b8f42e66c4d26
Prosper <anosikeprosper2@gmail.com>
2023-06-28 06:05:09 +0100

    removed all swagger ui related code

--- src/app.ts
+++ src/app.ts
@@ -11,13 +11,6 @@ const globalError = require('./errors/errorHandler')
 import cache from "./config/cache";
 require("dotenv").config();
 
-
-
-
-import * as yaml from 'yaml';
-import swaggerJSDoc from "swagger-jsdoc";
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from "./swagger.json";
 import path from "path";
 
 
@@ -33,45 +26,11 @@ app.use(passport.initialize());
 
 
 if(process.env.NODE_ENV !=='test'){
-  console.log(process.env.NODE_ENV)
 
   app.use(limiter)
 }
 
-// const options = {
-//     definition: {
-//       openapi: "3.1.0",
-//       info: {
-//         title: "LogRocket Express API with Swagger",
-//         version: "0.1.0",
-//         description:
-//           "This is a simple CRUD API application made with Express and documented with Swagger",
-//         license: {
-//           name: "MIT",
-//           url: "https://spdx.org/licenses/MIT.html",
-//         },
-//         contact: {
-//           name: "LogRocket",
-//           url: "https://logrocket.com",
-//           email: "info@email.com",
-//         },
-//       },
-//       servers: [
-//         {
-//           url: "http://localhost:4000",
-//         },
-//       ],
-//     },
-//     apis: ["./routes/*.ts"],
-//   };
-  
-  // const specs = swaggerJSDoc(options);
-  // app.use(
-  //   "/api-docs",
-  //   swaggerUi.serve,
-  //   // swaggerUi.setup(specs)
-  //   swaggerUi.setup(swaggerDocument, { explorer: true })
-  // );
+
 
 connectToMongoDB();
 cache();
