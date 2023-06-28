0b3e03d65d27f770c8c77d4c380970a7a2eb2b63
Prosper <anosikeprosper2@gmail.com>
2023-06-28 06:08:42 +0100

    removed swagger ui code

--- src/routes/url.route.ts
+++ src/routes/url.route.ts
@@ -15,28 +15,8 @@ import {redirectToUrl} from '../controllers/redirect.controller'
 
 const router = Router();
 
-/**
- * @swagger
- *   post:
- *     summary: Create a new book
- *     tags: [Books]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Book'
- *     responses:
- *       200:
- *         description: The created book.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Book'
- *       500:
- *         description: Some server error
- *
- */
+
+
 router.post("/",urlValidatorMiddleware, verifyUser, createUrl);
 
 router.post('/qrcode',urlValidatorMiddleware, verifyUser, getqrCode )
